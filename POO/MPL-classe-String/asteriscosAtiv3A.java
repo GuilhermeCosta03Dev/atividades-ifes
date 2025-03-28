9d6007507241257d750aed6bf9f0ee5af7668162
Guilherme Cordeiro Costa <guilhermecosta030409@outlook.com>
2025-03-28 17:27:57 -0300

    asteriscosA

--- POO/MPL-classe-String/asteriscosAtiv3A.java
+++ POO/MPL-classe-String/asteriscosAtiv3A.java
@@ -1,8 +1,12 @@
 public class asteriscosAtiv3A {
     public static void main(String[] args) {
         String ast = "";
-        for (int i = 1 ; i<=15 ; i++) {
-            String 
+        for (int i = 1 ; i<=10 ; i++) {
+            for (int j = 1 ; j<=i ; j++) {
+                ast += "*";
+            }
+            System.out.println(ast);
+            ast = "";
         }
     }
 }
